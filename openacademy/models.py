94678e381445146ccb32ef9c14f819e8f7529c16
Daniel Seijas <seijasdeveloper@gmail.com>
2016-03-28 15:43:18 +0200

    Campos relacionados

--- openacademy/models.py
+++ openacademy/models.py
@@ -8,10 +8,17 @@ class openacademy(models.Model):
     name = fields.Char(String="Titulo", required=True)
     description = fields.Text()
 
+    responsible_id = fields.Many2one('res.users',
+    ondelete='set null', string="Responsable", index=True)
+
 class Session(models.Model):
     _name = 'openacademy.session'
- 
+
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+            ondelete='cascade', string="Curso", required=True)
